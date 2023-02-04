444999c2a569f58dd6fcdfd7d0f2ed3c683ed476
Akashdeep Singh <akash02ita@gmail.com>
2023-02-03 20:49:15 -0700

    ms1: token enums

--- ms1/lex.h
+++ ms1/lex.h
@@ -16,14 +16,43 @@ public:
 
   enum Token : int
   {
-    T_GT = 1,
+    T_BREAK = 1,
+    T_ELSE,
+    T_FOR,
+    T_FUNC,
+    T_IF,
+    T_RET,
+    T_VAR,
+
+    T_ADD,
+    T_SUB,
+    T_MULT,
+    T_DIV,
+    T_MOD,
+
+    T_AND,
+    T_OR,
+    T_EE,
+    T_LT,
+    T_GT,
     T_EQ,
+    T_EMARK,
+
+    T_NE,
+    T_LE,
     T_GE,
-    T_DOT,
-    T_BEGIN,
-    T_END,
-    T_IDENTIFIER,
-    T_STRING,
+
+    T_LP,
+    T_RP,
+    T_LC,
+    T_RC,
+
+    T_C, // comma
+    T_S, // semicolon
+
+    T_ID,
+    T_INT,
+    T_STRING
   };
 
   static char const *tokenToString(Token const);
