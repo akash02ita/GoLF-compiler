3868050e3b3c783c3930a1699f1d2d077b2f01fa
Akashdeep Singh <akash02ita@gmail.com>
2023-03-01 13:42:14 -0700

    ms2: add scanner header file lex.h this allows main function to print attributes or any other debugging info

--- ms2/main.c
+++ ms2/main.c
@@ -1,12 +1,15 @@
 #include "parse.tab.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "lex.h"
 
 int yylex();
 int main() {
     yytoken_kind_t token;
     while ((token = yylex()) != 0) {
-        fprintf(stdout, "Lexer parsed token %d\n", token);
+        fprintf(stdout, "Lexer parsed token %d", token);
+        fprintf(stdout, " with attribute %s", getAttribute());
+        fprintf(stdout, " at or near line %d\n", yylineno);
     }
     // if (yyparse() != 0) {
     //     return EXIT_FAILURE;
