7c3c9bb80be1119ea7dbcd209e620821c6082120
Akashdeep Singh <akash02ita@gmail.com>
2023-03-01 18:51:37 -0700

    ms2: checkpoint EBNF grammar to bison (just started) main idea is to do it at portion each time a new portion is done testing with `bison -Wcounterexamples parse.y` is ideal

--- ms2/main.c
+++ ms2/main.c
@@ -6,14 +6,14 @@
 int yylex();
 int main() {
     yytoken_kind_t token;
-    while ((token = yylex()) != 0) {
-        fprintf(stdout, "Lexer scanned token %s", tokenToString(token));
-        fprintf(stdout, " with attribute %s", getAttribute());
-        fprintf(stdout, " at or near line %d\n", yylineno);
+    // while ((token = yylex()) != 0) {
+    //     fprintf(stdout, "Lexer scanned token %s", tokenToString(token));
+    //     fprintf(stdout, " with attribute %s", getAttribute());
+    //     fprintf(stdout, " at or near line %d\n", yylineno);
 
-        lastToken = token;
-    }
-    // if (yyparse() != 0) {
-    //     return EXIT_FAILURE;
+    //     lastToken = token;
     // }
+    if (yyparse() != 0) {
+        return EXIT_FAILURE;
+    }
 }
