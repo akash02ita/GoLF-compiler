547145ae5de0fd75535c77c76b15db132ac6f1c6
Akashdeep Singh <akash02ita@gmail.com>
2023-03-01 17:19:00 -0700

    ms1: allowing token type variation for scanner or parser if using program without bison then custom token Token type will be used if using bison, then bison can generate header file that expects lexer to return tokens of type yytoken_kind_t global.h header file is used for this purpose to mantain control of compilation

--- ms1/lex.h
+++ ms1/lex.h
@@ -2,6 +2,54 @@
 
 #include <iostream>
 #include <string>
+#include "global.h"
+
+#if MODE == SCANNER_MODE
+// best to keep it outside class (stays consistent with C) and allows to easily cast G_enum_tok_t in main.cpp
+enum Token : int
+{
+  T_BREAK = 1,
+  T_ELSE,
+  T_FOR,
+  T_FUNC,
+  T_IF,
+  T_RET,
+  T_VAR,
+
+  T_ADD,
+  T_SUB,
+  T_MULT,
+  T_DIV,
+  T_MOD,
+
+  T_AND,
+  T_OR,
+  T_EE,
+  T_LT,
+  T_GT,
+  T_EQ,
+  T_EMARK,
+
+  T_NE,
+  T_LE,
+  T_GE,
+
+  T_LP,
+  T_RP,
+  T_LC,
+  T_RC,
+
+  T_C, // comma
+  T_S, // semicolon
+
+  T_ID,
+  T_INT,
+  T_STRING
+};
+#elif MODE == PARSER_MODE
+// include dependencies for parser code (bison header generated file)
+#include "parse.tab.h" // this let's compiler know where G_enum_tok_t is coming from
+#endif
 
 class MyFlexLexer : public yyFlexLexer
 {
@@ -13,50 +61,8 @@ public:
   int yylex();
   
   std::string getAttribute();
-
-  enum Token : int
-  {
-    T_BREAK = 1,
-    T_ELSE,
-    T_FOR,
-    T_FUNC,
-    T_IF,
-    T_RET,
-    T_VAR,
-
-    T_ADD,
-    T_SUB,
-    T_MULT,
-    T_DIV,
-    T_MOD,
-
-    T_AND,
-    T_OR,
-    T_EE,
-    T_LT,
-    T_GT,
-    T_EQ,
-    T_EMARK,
-
-    T_NE,
-    T_LE,
-    T_GE,
-
-    T_LP,
-    T_RP,
-    T_LC,
-    T_RC,
-
-    T_C, // comma
-    T_S, // semicolon
-
-    T_ID,
-    T_INT,
-    T_STRING
-  };
-
-  Token lastToken = (Token) 0; // default last token value: semicolon should not affect program
-  static char const *tokenToString(Token const);
+  G_enum_tok_t lastToken = (G_enum_tok_t) 0; // default last token value: semicolon should not affect program
+  static char const *tokenToString(G_enum_tok_t const);
   void myUnput(const char * text);
   void myUnput(const char * text, int si, int ei);
   bool handleImplicitSemicolon(std::string condition, std::string input);
--- ms1/main.cpp
+++ ms1/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
   while ((tk = lexer.yylex()) != 0)
   {
     std::cout
-        << "Token `" << MyFlexLexer::tokenToString(static_cast<MyFlexLexer::Token>(tk))
+        << "Token `" << MyFlexLexer::tokenToString(static_cast<G_enum_tok_t>(tk))
         << "` with attribute `"
         << lexer.getAttribute()
         << "`"
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
 
     // update last token
     // std::cout << "\t\tlast token was " << MyFlexLexer::tokenToString(lexer.lastToken) << std::endl;
-    lexer.lastToken = (MyFlexLexer::Token)tk;
+    lexer.lastToken = (G_enum_tok_t) tk;
   }
   return EXIT_SUCCESS;
 }
